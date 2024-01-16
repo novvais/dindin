3b63593363553f76c42844eac29f9e026c315bd4
Eduardo Novais <eduardodepaulanovais10@gmail.com>
2024-01-16 14:48:59 -0300

    FT

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 require("dotenv").config()
 const express = require("express");
-const rota = require("./routes/rotas");
+const route = require("./routes/route");
 
 const app = express();
 
 app.use(express.json());
-app.use(rota)
+app.use(route)
 
-app.listen(3000);
+app.listen(process.env.PORT);
